836dfffcb05e95cd8abe0fc3be99ebff20aac02e
keerthiv1119 <44571328+keerthiv1119@users.noreply.github.com>
2019-08-08 12:48:15 +0530

    Update Single_Linked_List.c

--- Single_Linked_List.c
+++ Single_Linked_List.c
@@ -198,6 +198,21 @@ void search()
 		printf("Element not found");
 	}
 }
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2)
+{
+    if(head1 == NULL && head2 == NULL)
+    {
+        return 1;
+    }
+    else if(head1 == NULL || head2 == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return (head1->data == head2->data) && compare_lists(head1->next,head2->next);
+    }
+}
 void reverse()
 {
 	struct node *current = head;
