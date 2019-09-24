f6cc5d204757f414ecaa7917fc04f38803241ed1
keerthiv1119 <44571328+keerthiv1119@users.noreply.github.com>
2019-09-24 12:35:52 +0530

    Update Quick_sort.c

--- Quick_sort.c
+++ Quick_sort.c
@@ -3,61 +3,52 @@ Author Name : Keerthi
 ID : N!51119
 Date : 18-09-2019
 */
-
-
-void quicksort(int arr[25],int L_Bound,int U_Bound){
-
-   int start, end, pivot, temp;
-
-   if(L_Bound < U_Bound)
-   {
-      pivot = L_Bound;
-      start = L_Bound;
-      end = U_Bound;
-
-      while(start < end)
-      {
-         while(arr[start] <= arr[pivot] && start < U_Bound)
-            start++;
-         while(arr[end] > arr[pivot])
-            end--;
-         if(start < end)
-         {
-            temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-         }
-      }
-      //swapping of the elements if the starting index crosses the ending index
-      temp = arr[pivot];
-      arr[pivot] = arr[end];
-      arr[end] = temp;
-      quicksort(arr,L_Bound,pivot-1);//sorting of the elements left to the pivot
-      quicksort(arr,pivot+1,U_Bound);//sorting of the elements right to the pivot
-   }
- }
-
 #include<stdio.h>
+
+int partition(int arr[],int low,int high)
+{
+	int i = (low - 1);
+	int pivot = arr[high];
+	int j, temp , temp1;
+	for(j = low;j <= high-1;j++)
+	{
+		if(arr[j] < pivot)
+		{
+			i = i+1;
+			temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
+		}
+	}
+	temp1 = arr[i+1];
+	arr[i+1] = arr[high];
+	arr[high] = temp1;
+	return (i+1);
+}
+
+void Quick_Sort(int arr[],int low,int high)
+{
+	int pivot_index;
+	if(low < high)
+	{
+		pivot_index = partition(arr,low,high);
+		Quick_Sort(arr, low, pivot_index - 1);
+		Quick_Sort(arr, pivot_index + 1, high);
+	}
+}
 int main()
 {
-  int i,array_size;
-  printf("Enter the size of the array : ");
-  scanf("%d",&array_size);
-  int array[array_size];
-  printf("Enter the array elements : \n");
-  for(i = 0;i < array_size;i++)
-  {
-    scanf("%d",&array[i]);
-  }
-  for(i = 0;i < array_size;i++)
-  {
-    printf("%d\t",array[i]);
-  }
-  quicksort(array , 0, array_size - 1);
-  printf("\n Sorted elements are: \n");
-  for(i = 0 ; i < array_size ; i++)
-  {
-      printf("%d \t",array[i]);
-  }
-  return 0;
+	int i,n;
+	printf("Enter the Size of the array");
+	scanf("%d",&n);
+	int array[n];
+	for(i = 0 ;i < n; i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	Quick_Sort(array , 0 , n-1);
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",array[i]);
+	}
 }
